4a7ccfbd2e72c920a2ea31615088c0e62ede64df
miltonvincenttis <milton.vincenttis@gmail.com>
2020-07-21 21:20:43 -0300

    renomeado

---
+++ lib/sumario.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+
+class Tela2 extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+      return Scaffold(
+         appBar: AppBar(
+            title: Text(TELA_TITULO),
+         ),
+         body: Center(
+         ),
+      );
+   }
+}
