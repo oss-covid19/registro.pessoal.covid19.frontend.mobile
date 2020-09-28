bd38813dc0e79236dbb8c1b9d0698aeb8bda6b33
miltonvincenttis <milton.vincenttis@gmail.com>
2020-09-28 16:15:08 -0300

    melhorado msg erro SENHA_ERRO, criado SENHA_DEVE_CRIAR

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -22,11 +22,18 @@ const String TEXT_BOTTOM = 'A informação é anônima nenhum dado a mais é obt
 /// Utilizado em :
 /// => entrar_validacao.dar
 /// => esqueceu_senha_model.dart
-///
+/// =>
 const String EMAIL_ERRO = "Email deve ser um email valido";
 const String EMAIL_VAZIO = "Você deve informar um email valido";
-const String SENHA_ERRO = "A senha deve ter min 4 characteres";
+const String SENHA_ERRO = "A senha deve ter min 4 digitos/alfanumericos";
 const String SENHA_VAZIO = "Você deve informar sua senha";
+const String SENHA_NAO_IGUAIS = "As senhas são diferentes";
+
+///
+/// utilizado em:
+/// => inscricao_xxx
+///
+const String SENHA_DEVE_CRIAR = "Você tem que criar uma senha de 4-6 digitos alfanuméricos";
 
 
 ShapeBorder globalDefineButtonShape() {
